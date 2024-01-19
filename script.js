b0c827c7194897b534333ec7dc8eda1b26cf4034
leonardoolia <leonardooliaa@gmail.com>
2024-01-19 14:45:58 +0100

    added h1 content and image content

--- script.js
+++ script.js
@@ -1,1 +1,13 @@
-console.log('js ok', Vue)
+const { createApp } = Vue;
+
+const app = createApp({
+    name: 'MiaApp',
+    data() {
+        return {
+            message: 'Ciao Mondo',
+            img: 'https://lwfiles.mycourse.app/6368e5089f20781a7e4f1805-public/2c162927114072f9ebbf04043a593fb9.png',
+        }
+    }
+});
+
+app.mount('#root');
